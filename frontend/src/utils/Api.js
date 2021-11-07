df0d71ed3e65e853dd8b4dfe6456ea0ef69064ba
trufan0ff <trufan.off@yandex.ru>
2021-11-07 17:51:53 +0300

    версия 1.8

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -125,6 +125,6 @@ class Api {
 }
 
 const api = new Api({
-    baseUrl: 'api.sunrise-mesto.nomoredomains.rocks',
+    baseUrl: 'https://api.sunrise-mesto.nomoredomains.rocks',
 })
 export default api;

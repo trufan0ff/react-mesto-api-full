28d541da05635a8961978f42ba60d7960bdcdbd9
trufan0ff <trufan.off@yandex.ru>
2021-11-14 09:52:33 +0300

    версия 1.8.8

--- backend/app.js
+++ backend/app.js
@@ -30,27 +30,27 @@ const allowedCors = [
   'http://sunrise-mesto.nomoredomains.icu',
 ];
 
-app.use(function (req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  next();
-});
+// app.use(function (req, res, next) {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых 
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+//   next();
+// });
 
 
 const { PORT = 3000 } = process.env;
@@ -67,23 +67,23 @@ const app = express();
 //   origin:'sunrise-mesto.nomoredomains.icu',
 // }));
 
-// const options = {
-//   origin: [
-//     'http://localhost:3000',
-//     'http://localhost:3001',
-//     'https://api.sunrise-mesto.nomoredomains.rocks',
-//     'http://api.sunrise-mesto.nomoredomains.rocks',
-//     'https://sunrise-mesto.nomoredomains.icu',
-//     'http://sunrise-mesto.nomoredomains.icu',
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://api.sunrise-mesto.nomoredomains.rocks',
+    'http://api.sunrise-mesto.nomoredomains.rocks',
+    'https://sunrise-mesto.nomoredomains.icu',
+    'http://sunrise-mesto.nomoredomains.icu',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
-// app.use('*', cors(options));
+app.use('*', cors(options));
 
 app.use(requestLogger); // подключаем логгер запросов
 

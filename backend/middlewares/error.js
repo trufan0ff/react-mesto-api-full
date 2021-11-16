7f520e6f349350a92d99b2612aecebc5bde932a7
trufan0ff <trufan.off@yandex.ru>
2021-11-16 13:22:02 +0300

    версия 1.9.6

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,7 +1,8 @@
-module.exports = (error, req, res) => {
+module.exports = (error, req, res, next) => {
   if (!error.statusCode || error.statusCode === 500) {
     res.status(500).send({ message: "Ошибка сервера" });
   } else {
     res.status(error.statusCode).send({ message: error.message });
   }
+  return next();
 };

82ec309a0f2cceecae1362abca0cb866781acbf7
trufan0ff <trufan.off@yandex.ru>
2021-11-16 09:45:07 +0300

    версия 1.9.6

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,6 +1,7 @@
 const Card = require("../models/card");
 const NotFoundError = require("../errors/not-found-err");
 const Forbidden = require("../errors/forbidden");
+const ValidationError = require("../errors/validation-error");
 
 // eslint-disable-next-line func-names
 const cathIdError = function (res, card) {
@@ -12,30 +13,60 @@ const cathIdError = function (res, card) {
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((card) => res.status(200).send({ card }))
+    .then((cards) => {
+      if (!cards) {
+        throw new NotFoundError("Данные о карточках не найдены!");
+      }
+      res.send(cards);
+    })
     .catch(next);
 };
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
-  const owner = req.user._id;
-  return Card.create({ name, link, owner })
-    .then((card) => res.status(201).send({ data: card }))
-    .catch(next);
+  Card.create({ name, link, owner: req.user._id })
+    .then((newCard) => {
+      if (!newCard) {
+        throw new NotFoundError("Неправильно переданы данные");
+      } else {
+        res.send(newCard);
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(
+          new ValidationError("Ошибка валидации. Введены некорректные данные"),
+        );
+      }
+      next(err);
+    });
 };
 
-module.exports.deleteCard = (req, res, next) => Card.findById(req.params.cardId)
-  .then((card) => {
-    if (!card) {
-      throw new NotFoundError("Данные не найдены");
-    }
-    if (req.user._id !== card.owner.toString()) {
-      throw new Forbidden("Доступ запрещён");
-    }
-    Card.findByIdAndRemove(req.params.cardId)
-      .then(res.status(200).send({ message: "Удалено" }));
-  })
-  .catch(next);
+module.exports.deleteCard = (req, res, next) => {
+  Card.findById(req.params._id)
+    .select("+owner")
+    .orFail(() => {
+      throw new NotFoundError(
+        "Карточки с таким id не существует, невозможно удалить!",
+      );
+    })
+    .then((card) => {
+      if (card.owner.toString() !== req.user._id) {
+        throw new Forbidden("Нельзя удалить чужую карточку!");
+      }
+    })
+    .then(() => {
+      Card.findByIdAndRemove(req.params._id)
+        .then((card) => {
+          if (!card) {
+            throw new NotFoundError("Запрашиваемый ресурс не найден");
+          }
+          res.send(card);
+        })
+        .catch(next);
+    })
+    .catch(next);
+};
 
 module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,

975167d1985a031098e7334474b69610ed2ba111
trufan0ff <trufan.off@yandex.ru>
2021-11-18 20:07:32 +0300

    версия 1.9.7

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -63,7 +63,7 @@ function App() {
                 .catch((err) => {
                     console.log(err)
                 });
-        },[])
+        },[currentUser])
 
     useEffect(() => {
         api.getUserInfo()
